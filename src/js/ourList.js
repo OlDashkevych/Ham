6b95dbecc2e78f3db40e4156122b3b9ac7c043d1
OlDashkevych <dashkevich672@gmail.com>
2020-05-07 19:09:21 +0300

    first

---
+++ src/js/ourList.js
@@ -0,0 +1,11 @@
+// import items from './ourListItems.json';
+// const insertingElement = document.querySelector('.ourServices__titles');
+// console.log(aaaa);
+// const createList = () => {
+//   const list = document.createElement('ul');
+//   list.classList.add('ourServices__list');
+//   console.log(insertingElement);
+//   console.log(items);
+// };
+// createList();
+console.log('sdfd');
